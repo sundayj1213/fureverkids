1aa822292b9adb75a3c4736752d519d0bc817532
Sunday Johnson <sundayjohnson1213@gmail.com>
2022-06-25 14:40:25 +0100

    Production Ready

--- components/template/Header.js
+++ components/template/Header.js
@@ -6,17 +6,13 @@ export default function Header({categories}) {
 
   return (
     <header className='bg-white'>
-      <nav className="navbar navbar-expand-lg navbar-light bg-white">
-        <div className="container">
-          <a className="navbar-brand" href="/">
+      <div className="container p-2">
+        <div className="menu d-flex align-items-center">
+          <a href="/">
             <Image src="/logo.png" width={60} height={60} />
           </a>
-          <svg className="navbar-toggler p-0 border-0" width="40" height="40" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <path d="M26.25 22.5H3.75V20H26.25V22.5ZM26.25 16.25H3.75V13.75H26.25V16.25ZM26.25 10H3.75V7.5H26.25V10Z" fill="#333333"/>
-          </svg>
-
-          <div className="collapse navbar-collapse" id="navbarNavDropdown">
-            <ul className="navbar-nav w-100 justify-content-evenly">
+          <div className="container bg-white" id="navbarNavDropdown">
+            <ul className="col-12 navbar-nav menu-items bg-white border px-2">
               <li className="nav-item">
                 <a 
                   className={!router.asPath.includes('category') ? 'nav-link active': 'nav-link'} 
@@ -31,11 +27,13 @@ export default function Header({categories}) {
                   </li>
                 ))
               }
-             
             </ul>
           </div>
+          <svg className="navbar-toggler p-0 border-0" width="40" height="40" viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <path d="M26.25 22.5H3.75V20H26.25V22.5ZM26.25 16.25H3.75V13.75H26.25V16.25ZM26.25 10H3.75V7.5H26.25V10Z" fill="#333333"/>
+          </svg>   
         </div>
-      </nav>
+      </div>
     </header>
   )
 }
